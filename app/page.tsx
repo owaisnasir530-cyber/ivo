2fb00f1258fe632dd88c43fa70096159404a68c5
owaisnasir530-cyber <owaisnasir530@gmail.com>
2025-12-23 10:47:52 +0500

    Improve responsive layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,8 @@ import App from "./App";
 
 export default function Page() {
   return (
-    <div className="min-h-screen flex items-center justify-center p-4">
-      <div className="w-full max-w-5xl">
-        <App />
-      </div>
+    <div className="h-screen w-full overflow-hidden">
+      <App />
     </div>
   );
 }
